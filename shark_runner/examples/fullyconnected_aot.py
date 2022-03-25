5b2badd4f2f4f3a474b966d9f612796f528455cb
Prashant Kumar <prashant@nod-labs.com>
2022-03-25 12:55:31 +0000

    Fix bug related to aot module.
    Single element is returned as is and tuple is not created for that.

--- shark_runner/examples/fullyconnected_aot.py
+++ shark_runner/examples/fullyconnected_aot.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torchvision.models as models
 from shark_runner import SharkInference, SharkTrainer
 
 
@@ -9,7 +10,7 @@ class NeuralNet(nn.Module):
         self.l1 = nn.Linear(10, 16)
         self.relu = nn.ReLU()
         self.l2 = nn.Linear(16, 2)
-        self.train(False)
+        self.train(True)
 
     def forward(self, x):
         out = self.l1(x)
@@ -25,9 +26,27 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 
 input = torch.randn(10, 10)
-labels = torch.randn(1, 2)
+labels = torch.randn(10, 2)
 
-shark_module = SharkInference(NeuralNet(), (input,), from_aot=True)
-results = shark_module.forward((input,))
+# shark_module = SharkInference(NeuralNet(), (input,), from_aot=True)
+# results = shark_module.forward((input,))
 
-print(results)
+shark_module = SharkTrainer(NeuralNet(), (input,), (labels,), from_aot=True)
+results = shark_module.train((input,))
+
+# print(results)
+
+input = torch.randn(1, 3, 224, 224)
+labels = torch.randn(1, 1000)
+
+class Resnet50Module(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.resnet = models.resnet50(pretrained=True)
+        self.train(True)
+
+    def forward(self, img):
+        return self.resnet.forward(img)
+
+shark_module = SharkTrainer(Resnet50Module(), (input,), (labels,), from_aot=True)
+results = shark_module.train((input,))
