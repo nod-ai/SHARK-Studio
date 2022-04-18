944a9c91674cd878f32d31c78d54e0f887a93550
Prashant Kumar <prashant@nod-labs.com>
2022-04-18 05:57:31 +0000

    Modify install.sh and update readme.

---
+++ setup_venv.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Sets up a venv suitable for running samples.
+# Recommend getting default 'python' to be python 3. For example on Debian:
+#   sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+# Or launch with python=/some/path
+TD="$(cd $(dirname $0) && pwd)"
+VENV_DIR="$TD/shark.venv"
+if [ -z "$PYTHON" ]; then
+  PYTHON="$(which python)"
+fi
+
+echo "Setting up venv dir: $VENV_DIR"
+echo "Python: $PYTHON"
+echo "Python version: $("$PYTHON" --version)"
+
+function die() {
+  echo "Error executing command: $*"
+  exit 1
+}
+
+$PYTHON -m venv "$VENV_DIR" || die "Could not create venv."
+source "$VENV_DIR/bin/activate" || die "Could not activate venv"
+
+# Upgrade pip and install requirements. 'python' is used here in order to
+# reference to the python executable from the venv.
+python -m pip install --upgrade pip || die "Could not upgrade pip"
+python -m pip install --upgrade -r "$TD/requirements.txt"
+pip install --find-links https://github.com/llvm/torch-mlir/releases torch-mlir
+pip install --find-links https://github.com/NodLabs/SHARK/releases iree-compiler iree-runtime
+pip install git+https://github.com/pytorch/functorch.git
+pip install transformers
+pip install .
+
+Red=`tput setaf 1`          
+Green=`tput setaf 2`
+echo "${Green}Before running examples activate venv with:"
+echo "  ${Red}source $VENV_DIR/bin/activate"
+
