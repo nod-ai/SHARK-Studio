6cd237ea6de156f27fae3807d615537317b00620
monorimet <garveyej@gmail.com>
2022-05-20 23:41:12 -0500

    Add support for TensorFlow modules + TF miniLM example
    Fix inference forward and train loop for Tensorflow.
    
    Separate TF dependencies from normal torch SharkRunner.
    
    Remove leftover tf function from iree_utils.py

---
+++ shark/examples/minilm_tf.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from transformers import BertModel, BertTokenizer, TFBertModel
+from shark.shark_runner_tf import SharkInferenceTF
+MAX_SEQUENCE_LENGTH = 512
+BATCH_SIZE = 1
+
+# Create a set of 2-dimensional inputs
+bert_input = [tf.TensorSpec(shape=[BATCH_SIZE,MAX_SEQUENCE_LENGTH],dtype=tf.int32),
+            tf.TensorSpec(shape=[BATCH_SIZE,MAX_SEQUENCE_LENGTH], dtype=tf.int32),
+            tf.TensorSpec(shape=[BATCH_SIZE,MAX_SEQUENCE_LENGTH], dtype=tf.int32)]
+
+class BertModule(tf.Module):
+    def __init__(self):
+        super(BertModule, self).__init__()
+        # Create a BERT trainer with the created network.
+        self.m = TFBertModel.from_pretrained("microsoft/MiniLM-L12-H384-uncased", from_pt=True)
+
+        # Invoke the trainer model on the inputs. This causes the layer to be built.
+        self.m.predict = lambda x,y,z: self.m.call(input_ids=x, attention_mask=y, token_type_ids=z, training=False)
+
+    @tf.function(input_signature=bert_input)
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        return self.m.predict(input_ids, attention_mask, token_type_ids)
+
+if __name__ == "__main__":
+    # Prepping Data
+    tokenizer = BertTokenizer.from_pretrained("microsoft/MiniLM-L12-H384-uncased")
+    text = "Replace me by any text you'd like."
+    encoded_input = tokenizer(text, padding='max_length', truncation=True, max_length=MAX_SEQUENCE_LENGTH)
+    for key in encoded_input:
+        encoded_input[key] = tf.expand_dims(tf.convert_to_tensor(encoded_input[key]),0)
+
+    shark_module = SharkInferenceTF(
+    	BertModule(),
+    	(encoded_input["input_ids"], encoded_input["attention_mask"], encoded_input["token_type_ids"])
+    )
+    
+    shark_module.benchmark_forward((encoded_input["input_ids"], encoded_input["attention_mask"], encoded_input["token_type_ids"]))
---
+++ shark/shark_runner_tf.py
@@ -0,0 +1,151 @@
+# Copyright 2020 The Nod Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from shark.iree_utils import get_results
+from shark.iree_utils_tf import get_iree_compiled_module_tf, export_tf_iree_module_to_vmfb
+import os
+from shark.functorch_utils import AOTModule
+from shark.parser import shark_args
+from shark.backward_makefx import MakeFxModule
+from tqdm import tqdm
+import time
+
+
+class SharkRunnerTF:
+    """Base class for Shark Inference and Shark Runner."""
+
+    def __init__(
+        self,
+        model,
+        input: tuple,
+        dynamic: bool,
+        device: str,
+        tracing_required: bool,
+        from_aot: bool,
+    ):
+        self.tf_module = model
+        self.input = input
+        if shark_args.save_vmfb:
+            export_tf_iree_module_to_vmfb(self.tf_module,
+                                              shark_args.repro_dir)
+        (
+            self.iree_compilation_module,
+            self.iree_config,
+        ) = get_iree_compiled_module_tf(self.tf_module, device)
+
+    def forward(self, input):
+        return get_results(self.iree_compilation_module, input,
+                           self.iree_config)
+
+
+class SharkInferenceTF:
+    """TODO: Write the description"""
+
+    def __init__(
+        self,
+        model,
+        input: tuple,
+        dynamic: bool = False,
+        device: str = None,
+        jit_trace: bool = False,
+        from_aot: bool = False,
+        custom_inference_fn=None,
+    ):
+        self.model = model
+        self.input = input
+        self.from_aot = from_aot
+
+        self.device = device if device is not None else shark_args.device
+
+        if from_aot:
+            aot_module = AOTModule(model,
+                                   input,
+                                   custom_inference_fn=custom_inference_fn)
+            aot_module.generate_inference_graph()
+            self.model = aot_module.forward_graph
+            self.input = aot_module.forward_inputs
+
+        self.shark_runner = SharkRunnerTF(self.model, self.input, dynamic,
+                                        self.device, jit_trace, from_aot)
+
+    def benchmark_forward(self, inputs):
+        inputs = self.input if self.from_aot else inputs
+        input_list = [x.numpy() for x in inputs]
+        for i in range(shark_args.num_warmup_iterations):
+            self.shark_runner.forward(input_list)
+
+        for i in range(shark_args.num_iterations):
+            begin = time.time()
+            out = self.shark_runner.forward(input_list)
+            end = time.time()
+            print("Iteration " + str(i) + ": " + str(end - begin))
+            if i == shark_args.num_iterations - 1:
+                return out
+
+    def forward(self, inputs):
+        # TODO Capture weights and inputs in case of AOT, Also rework the
+        # forward pass.
+        inputs = self.input if self.from_aot else inputs
+        input_list = [x.numpy() for x in inputs]
+        return self.shark_runner.forward(input_list)
+
+
+class SharkTrainerTF:
+    """TODO: Write the description"""
+
+    def __init__(
+        self,
+        model,
+        input: tuple,
+        dynamic: bool = False,
+        device: str = None,
+        jit_trace: bool = False,
+        from_aot: bool = True,
+        custom_inference_fn=None,
+    ):
+        self.model = model
+        self.input = []
+        self.from_aot = from_aot
+
+        self.device = device if device is not None else shark_args.device
+
+        aot_module = MakeFxModule(model,
+                                  input,
+                                  custom_inference_fn=custom_inference_fn)
+        aot_module.generate_graph()
+        self.model = aot_module.backward_graph
+
+        self.weights = [
+            i[1] for i in sorted(dict(model.named_parameters()).items())
+        ]
+        for i in sorted(dict(model.named_buffers()).items()):
+            self.weights.append(i[1])
+
+        for i in input:
+            self.input.append(i)
+
+        self.shark_runner = SharkRunnerTF(self.model, self.weights + self.input,
+                                        dynamic, self.device, jit_trace,
+                                        from_aot)
+
+    def train(self, num_iters=1):
+        """Returns the updated weights after num_iters"""
+        weights = [x.numpy() for x in self.weights]
+        inputs = [x.numpy() for x in self.input]
+
+        print(f"Training started for {num_iters} iterations:")
+        for i in tqdm(range(num_iters)):
+            weights = self.shark_runner.forward(weights + inputs)
+
+        return weights
