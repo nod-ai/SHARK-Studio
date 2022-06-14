ec13200c7c0959a635f77c7950878c04677da12c
Ean Garvey <87458719+monorimet@users.noreply.github.com>
2022-06-13 19:51:21 -0500

    Update conftest.py

--- tank/conftest.py
+++ tank/conftest.py
@@ -1,3 +1,3 @@
 def pytest_addoption(parser):
     # Attaches SHARK command-line arguments to the pytest machinery.
-    parser.addoption("--save_mlir", action="store_true", help="Pass option to save input MLIR module to /tmp/ directory.")
+    parser.addoption("--save_mlir", action="store_true", default="False", help="Pass option to save input MLIR module to /tmp/ directory.")
