1322ec59356ea28418f2e709f38ba80536458ab0
Phaneesh Barwaria <b.phaneesh@gmail.com>
2022-08-25 10:24:56 +0530

    Simplified Testing Interface (#289)

--- tank/electra-small-discriminator_tf/electra-small-discriminator_tf_test.py
+++ tank/electra-small-discriminator_tf/electra-small-discriminator_tf_test.py
@@ -1,6 +1,8 @@
 from shark.iree_utils._common import check_device_drivers, device_driver_info
 from shark.shark_inference import SharkInference
 from shark.shark_downloader import download_tf_model
+from tank.test_utils import get_valid_test_params, shark_test_name_func
+from parameterized import parameterized
 
 import iree.compiler as ireec
 import unittest
@@ -34,25 +36,10 @@ class ElectraModuleTest(unittest.TestCase):
         self.module_tester = ElectraModuleTester(self)
         self.module_tester.benchmark = pytestconfig.getoption("benchmark")
 
-    def test_module_static_cpu(self):
-        dynamic = False
-        device = "cpu"
-        self.module_tester.create_and_check_module(dynamic, device)
+    param_list = get_valid_test_params()
 
-    @pytest.mark.skipif(
-        check_device_drivers("gpu"), reason=device_driver_info("gpu")
-    )
-    def test_module_static_gpu(self):
-        dynamic = False
-        device = "gpu"
-        self.module_tester.create_and_check_module(dynamic, device)
-
-    @pytest.mark.skipif(
-        check_device_drivers("vulkan"), reason=device_driver_info("vulkan")
-    )
-    def test_module_static_vulkan(self):
-        dynamic = False
-        device = "vulkan"
+    @parameterized.expand(param_list, name_func=shark_test_name_func)
+    def test_module(self, dynamic, device):
         self.module_tester.create_and_check_module(dynamic, device)
 
 
--- tank/mobilenet_v3_small_torch/mobilenet_v3_small_torch_test.py
+++ tank/mobilenet_v3_small_torch/mobilenet_v3_small_torch_test.py
@@ -1,6 +1,8 @@
 from shark.shark_inference import SharkInference
 from shark.iree_utils._common import check_device_drivers, device_driver_info
 from shark.shark_downloader import download_torch_model
+from tank.test_utils import get_valid_test_params, shark_test_name_func
+from parameterized import parameterized
 
 import unittest
 import numpy as np
@@ -56,49 +58,15 @@ class MobileNetV3ModuleTest(unittest.TestCase):
         self.module_tester = MobileNetV3ModuleTester(self)
         self.module_tester.benchmark = pytestconfig.getoption("benchmark")
 
-    def test_module_static_cpu(self):
-        dynamic = False
-        device = "cpu"
-        self.module_tester.create_and_check_module(dynamic, device)
+    param_list = get_valid_test_params()
 
-    def test_module_dynamic_cpu(self):
-        dynamic = True
-        device = "cpu"
-        self.module_tester.create_and_check_module(dynamic, device)
-
-    @pytest.mark.xfail(reason="golden results don't match.")
-    @pytest.mark.skipif(
-        check_device_drivers("gpu"), reason=device_driver_info("gpu")
-    )
-    def test_module_static_gpu(self):
-        dynamic = False
-        device = "gpu"
-        self.module_tester.create_and_check_module(dynamic, device)
-
-    @pytest.mark.xfail(reason="golden results don't match.")
-    @pytest.mark.skipif(
-        check_device_drivers("gpu"), reason=device_driver_info("gpu")
-    )
-    def test_module_dynamic_gpu(self):
-        dynamic = True
-        device = "gpu"
-        self.module_tester.create_and_check_module(dynamic, device)
-
-    @pytest.mark.xfail(reason="stuck in the pipeline.")
-    @pytest.mark.skipif(
-        check_device_drivers("vulkan"), reason=device_driver_info("vulkan")
-    )
-    def test_module_static_vulkan(self):
-        dynamic = False
-        device = "vulkan"
-        self.module_tester.create_and_check_module(dynamic, device)
-
-    @pytest.mark.skipif(
-        check_device_drivers("vulkan"), reason=device_driver_info("vulkan")
-    )
-    def test_module_dynamic_vulkan(self):
-        dynamic = True
-        device = "vulkan"
+    @parameterized.expand(param_list, name_func=shark_test_name_func)
+    def test_module(self, dynamic, device):
+        if device == "gpu":
+            pytest.xfail(reason="golden results don't match.")
+        elif device in ["vulkan", "metal"]:
+            if dynamic == False:
+                pytest.xfail(reason="stuck in the pipeline.")
         self.module_tester.create_and_check_module(dynamic, device)
 
 
