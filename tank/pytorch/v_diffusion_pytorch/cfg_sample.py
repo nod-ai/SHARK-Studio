cec0564f5be849250ec82d4d1264e6ecb4be2ea3
Quinn Dawkins <quinn.dawkins@gmail.com>
2022-09-13 14:21:53 -0400

    Clean up the v-diffusion install pipeline

--- tank/pytorch/v_diffusion_pytorch/cfg_sample.py
+++ tank/pytorch/v_diffusion_pytorch/cfg_sample.py
@@ -68,6 +68,12 @@ p.add_argument(
 p.add_argument("--checkpoint", type=str, help="the checkpoint to use")
 p.add_argument("--device", type=str, help="the device to use")
 p.add_argument(
+    "--runtime_device",
+    type=str,
+    help="the device to use with SHARK",
+    default="cpu",
+)
+p.add_argument(
     "--eta",
     type=float,
     default=0.0,
@@ -235,7 +241,7 @@ mlir_model = module
 func_name = "forward"
 
 shark_module = SharkInference(
-    mlir_model, func_name, device="cuda", mlir_dialect="linalg"
+    mlir_model, func_name, device=args.runtime_device, mlir_dialect="linalg"
 )
 shark_module.compile()
 
