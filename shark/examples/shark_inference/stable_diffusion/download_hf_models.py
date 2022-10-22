4f906a265cf9f4386cebabc9592b91c7238a953b
Anush Elangovan <anush@nod-labs.com>
2022-10-22 12:43:52 -0700

    Fix lint

--- shark/examples/shark_inference/stable_diffusion/download_hf_models.py
+++ shark/examples/shark_inference/stable_diffusion/download_hf_models.py
@@ -9,8 +9,17 @@ processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+inputs = processor(
+    text=["a photo of a cat", "a photo of a dog"],
+    images=image,
+    return_tensors="pt",
+    padding=True,
+)
 
 outputs = model(**inputs)
-logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
+logits_per_image = (
+    outputs.logits_per_image
+)  # this is the image-text similarity score
+probs = logits_per_image.softmax(
+    dim=1
+)  # we can take the softmax to get the label probabilities
