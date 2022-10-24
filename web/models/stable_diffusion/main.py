467fe14f1c5f563b15c25200a55745113153f97a
Gaurav Shukla <gaurav@nod-labs.com>
2022-10-24 13:53:38 +0530

    [WEB] Expose SD parameters in the web ui
    This commit exposes some parameters of the stable diffusion model in
    the web UI.
    
    Signed-Off-by: Gaurav Shukla <gaurav@nod-labs.com>

--- web/models/stable_diffusion/main.py
+++ web/models/stable_diffusion/main.py
@@ -1,7 +1,7 @@
 from transformers import CLIPTextModel, CLIPTokenizer
 import torch
 from PIL import Image
-from diffusers import LMSDiscreteScheduler
+from diffusers import DDIMScheduler, LMSDiscreteScheduler, PNDMScheduler
 from tqdm.auto import tqdm
 import numpy as np
 from models.stable_diffusion.model_wrappers import (
@@ -27,15 +27,29 @@ DEBUG = False
 class Arguments:
     def __init__(
         self,
-        prompt="a boy riding a bicycle",
-        steps=10,
-        precision="fp32",
-        device="cpu",
-        import_mlir=False,
-        max_length=77,
+        prompt: str,
+        scheduler: str,
+        iteration_count: int,
+        batch_size: int,
+        steps: int,
+        guidance: float,
+        height: int,
+        width: int,
+        seed: int,
+        precision: str,
+        device: str,
+        import_mlir: bool = False,
+        max_length: int = 77,
     ):
         self.prompt = prompt
+        self.scheduler = scheduler
+        self.iteration_count = iteration_count
+        self.batch_size = batch_size
         self.steps = steps
+        self.guidance = guidance
+        self.height = height
+        self.width = width
+        self.seed = seed
         self.precision = precision
         self.device = device
         self.import_mlir = import_mlir
@@ -43,9 +57,12 @@ class Arguments:
 
 
 def get_models():
+
+    global args
+
     if args.precision == "fp16":
         if args.import_mlir == True:
-            return get_unet16_wrapped(args), get_vae16(args)
+            return get_vae16(args), get_unet16_wrapped(args)
         return get_shark_model(args, GCLOUD_BUCKET, VAE_FP16), get_shark_model(
             args, GCLOUD_BUCKET, UNET_FP16
         )
@@ -66,7 +83,19 @@ def get_models():
     return None, None
 
 
-def stable_diff_inf(prompt: str, steps, precision, device: str):
+def stable_diff_inf(
+    prompt: str,
+    scheduler: str,
+    iteration_count: int,
+    batch_size: int,
+    steps: int,
+    guidance: float,
+    height: int,
+    width: int,
+    seed: str,
+    precision: str,
+    device: str,
+):
 
     global args
     global DEBUG
@@ -77,23 +106,61 @@ def stable_diff_inf(prompt: str, steps, precision, device: str):
     if log_write:
         DEBUG = True
 
-    args = Arguments(prompt, steps, precision, device)
+    # set seed value
+    if seed == "":
+        seed = int(torch.randint(low=25, high=100, size=()))
+    else:
+        try:
+            seed = int(seed)
+        except ValueError:
+            seed = hash(seed)
+
+    # set scheduler value
+    if scheduler == "PNDM":
+        scheduler = PNDMScheduler(
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            num_train_timesteps=1000,
+        )
+    elif scheduler == "LMS":
+        scheduler = LMSDiscreteScheduler(
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            num_train_timesteps=1000,
+        )
+    elif scheduler == "DDIM":
+        scheduler = DDIMScheduler(
+            beta_start=0.00085,
+            beta_end=0.012,
+            beta_schedule="scaled_linear",
+            clip_sample=False,
+            set_alpha_to_one=False,
+        )
+    else:
+        raise Exception(
+            f"Does not support scheduler with name {args.scheduler}."
+        )
+
+    args = Arguments(
+        prompt,
+        scheduler,
+        iteration_count,
+        batch_size,
+        steps,
+        guidance,
+        height,
+        width,
+        seed,
+        precision,
+        device,
+    )
     dtype = torch.float32 if args.precision == "fp32" else torch.half
-
-    prompt = [args.prompt]
-
-    height = 512  # default height of Stable Diffusion
-    width = 512  # default width of Stable Diffusion
-
     num_inference_steps = int(args.steps)  # Number of denoising steps
-
-    guidance_scale = 7.5  # Scale for classifier-free guidance
-
     generator = torch.manual_seed(
-        42
+        args.seed
     )  # Seed generator to create the inital latent noise
-
-    batch_size = len(prompt)
 
     vae, unet = get_models()
     tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
@@ -101,17 +168,8 @@ def stable_diff_inf(prompt: str, steps, precision, device: str):
         "openai/clip-vit-large-patch14"
     )
 
-    scheduler = LMSDiscreteScheduler(
-        beta_start=0.00085,
-        beta_end=0.012,
-        beta_schedule="scaled_linear",
-        num_train_timesteps=1000,
-    )
-
-    start = time.time()
-
     text_input = tokenizer(
-        prompt,
+        [args.prompt] * batch_size,
         padding="max_length",
         max_length=args.max_length,
         truncation=True,
@@ -131,7 +189,7 @@ def stable_diff_inf(prompt: str, steps, precision, device: str):
     text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
 
     latents = torch.randn(
-        (batch_size, 4, height // 8, width // 8),
+        (batch_size, 4, args.height // 8, args.width // 8),
         generator=generator,
         dtype=dtype,
     )
@@ -159,7 +217,7 @@ def stable_diff_inf(prompt: str, steps, precision, device: str):
         avg_ms += step_time
         step_ms = int((step_time) * 1000)
         if DEBUG:
-            log_write.write(f"time/itr={step_ms}ms")
+            log_write.write(f"time={step_ms}ms")
         latents = scheduler.step(noise_pred, i, latents)["prev_sample"]
 
     avg_ms = 1000 * avg_ms / args.steps
@@ -174,6 +232,7 @@ def stable_diff_inf(prompt: str, steps, precision, device: str):
     image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
     images = (image * 255).round().astype("uint8")
     pil_images = [Image.fromarray(image) for image in images]
+    print("total images:", len(pil_images))
     output = pil_images[0]
     # save the output image with the prompt name.
     output.save(os.path.join(output_loc))
