a901d44388f5bedc7e9c09c6d8feb8292f9cec79
Prashant Kumar <prashant@nod-labs.com>
2022-11-18 10:38:03 +0000

    Update stable diffusion models to point to new location.

--- shark/examples/shark_inference/stable_diffusion/opt_params.py
+++ shark/examples/shark_inference/stable_diffusion/opt_params.py
@@ -32,31 +32,31 @@ def get_unet():
             ]
             # TODO: Pass iree_flags to the exported model.
             if args.import_mlir:
-                return get_unet16_wrapped(model_name=model_name)
+                return get_unet16_wrapped(model_name, iree_flags)
             return get_shark_model(bucket, model_name, iree_flags)
         else:
             bucket = "gs://shark_tank/prashant_nod"
-            model_name = "unet_fp16_v2"
+            model_name = "unet_18nov_fp16"
             iree_flags += [
                 "--iree-flow-enable-padding-linalg-ops",
                 "--iree-flow-linalg-ops-padding-size=32",
                 "--iree-flow-enable-conv-nchw-to-nhwc-transform",
             ]
             if args.import_mlir:
-                return get_unet16_wrapped(model_name=model_name)
+                return get_unet16_wrapped(model_name, iree_flags)
             return get_shark_model(bucket, model_name, iree_flags)
 
     # Tuned model is not present for `fp32` case.
     if args.precision == "fp32":
         bucket = "gs://shark_tank/prashant_nod"
-        model_name = "unet_fp32_v2"
+        model_name = "unet_18nov_fp32"
         iree_flags += [
             "--iree-flow-enable-conv-nchw-to-nhwc-transform",
             "--iree-flow-enable-padding-linalg-ops",
             "--iree-flow-linalg-ops-padding-size=16",
         ]
         if args.import_mlir:
-            return get_unet32_wrapped(model_name=model_name)
+            return get_unet32_wrapped(model_name, iree_flags)
         return get_shark_model(bucket, model_name, iree_flags)
 
     if args.precision == "int8":
@@ -82,26 +82,26 @@ def get_vae():
         )
     if args.precision in ["fp16", "int8"]:
         bucket = "gs://shark_tank/prashant_nod"
-        model_name = "vae_fp16"
+        model_name = "vae_18nov_fp16"
         iree_flags += [
             "--iree-flow-enable-conv-nchw-to-nhwc-transform",
             "--iree-flow-enable-padding-linalg-ops",
             "--iree-flow-linalg-ops-padding-size=32",
         ]
         if args.import_mlir:
-            return get_vae16(model_name)
+            return get_vae16(model_name, iree_flags)
         return get_shark_model(bucket, model_name, iree_flags)
 
     if args.precision == "fp32":
         bucket = "gs://shark_tank/prashant_nod"
-        model_name = "vae_fp32"
+        model_name = "vae_18nov_fp32"
         iree_flags += [
             "--iree-flow-enable-conv-nchw-to-nhwc-transform",
             "--iree-flow-enable-padding-linalg-ops",
             "--iree-flow-linalg-ops-padding-size=16",
         ]
         if args.import_mlir:
-            return get_vae32(model_name)
+            return get_vae32(model_name, iree_flags)
         return get_shark_model(bucket, model_name, iree_flags)
 
 
@@ -112,11 +112,11 @@ def get_clip():
             f"-iree-vulkan-target-triple={args.iree_vulkan_target_triple}"
         )
     bucket = "gs://shark_tank/prashant_nod"
-    model_name = "clip_text"
+    model_name = "clip_18nov_fp32"
     iree_flags += [
         "--iree-flow-linalg-ops-padding-size=16",
         "--iree-flow-enable-padding-linalg-ops",
     ]
     if args.import_mlir:
-        return get_clipped_text(model_name)
+        return get_clipped_text(model_name, iree_flags)
     return get_shark_model(bucket, model_name, iree_flags)
