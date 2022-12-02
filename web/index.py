ebb60019aa3727b17ef981cfb6152386d94c0462
Ean Garvey <87458719+monorimet@users.noreply.github.com>
2022-12-02 12:47:31 -0600

    Minor formatting fix. (#538)

--- web/index.py
+++ web/index.py
@@ -12,10 +12,14 @@ from random import randint
 from numpy import iinfo
 import numpy as np
 
+
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    base_path = getattr(
+        sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__))
+    )
     return os.path.join(base_path, relative_path)
+
 
 prompt_examples = []
 prompt_loc = resource_path("prompts.json")
