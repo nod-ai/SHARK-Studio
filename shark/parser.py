816bc1335563c1c50a790a3bb8ee0afef677b335
Ean Garvey <ean@nod-labs.com>
2022-12-12 18:38:06 +0000

    Add guard to shark parser and add dict_configs to gitignore.

--- shark/parser.py
+++ shark/parser.py
@@ -33,83 +33,79 @@ def dir_file(path):
         )
 
 
-parser = argparse.ArgumentParser(description="SHARK runner.")
-parser.add_argument(
-    "--device",
-    type=str,
-    default="cpu",
-    help="Device on which shark_runner runs. options are cpu, cuda, and vulkan",
-)
-parser.add_argument(
-    "--repro_dir",
-    help="Directory to which module files will be saved for reproduction or debugging.",
-    type=dir_path,
-    default="./shark_tmp",
-)
-parser.add_argument(
-    "--enable_tf32",
-    type=bool,
-    default=False,
-    help="Enables TF32 precision calculations on supported GPUs.",
-)
-parser.add_argument(
-    "--model_config_path",
-    help="Directory to where the tuned model config file is located.",
-    default=None,
-)
-
-parser.add_argument(
-    "--num_warmup_iterations",
-    type=int,
-    default=5,
-    help="Run the model for the specified number of warmup iterations.",
-)
-parser.add_argument(
-    "--num_iterations",
-    type=int,
-    default=100,
-    help="Run the model for the specified number of iterations.",
-)
-parser.add_argument(
-    "--onnx_bench",
-    default=False,
-    action="store_true",
-    help="When enabled, pytest bench results will include ONNX benchmark results.",
-)
-parser.add_argument(
-    "--shark_prefix",
-    default="latest",
-    help="gs://shark_tank/<this_flag>/model_directories",
-)
-parser.add_argument(
-    "--update_tank",
-    default=False,
-    action="store_true",
-    help="When enabled, SHARK downloader will update local shark_tank if local hash is different from latest upstream hash.",
-)
-parser.add_argument(
-    "--local_tank_cache",
-    default="",
-    help="Specify where to save downloaded shark_tank artifacts. If this is not set, the default is ~/.local/shark_tank/.",
-)
-
-parser.add_argument(
-    "--dispatch_benchmarks",
-    default=None,
-    help='dispatches to return benchamrk data on.  use "All" for all, and None for none.',
-)
-
-parser.add_argument(
-    "--dispatch_benchmarks_dir",
-    default="temp_dispatch_benchmarks",
-    help='directory where you want to store dispatch data generated with "--dispatch_benchmarks"',
-)
-
-parser.add_argument(
-    "--enable_conv_transform",
-    default=False,
-    action="store",
-    help="Enables the --iree-flow-enable-conv-nchw-to-nhwc-transform flag.",
-)
-
-shark_args, unknown = parser.parse_known_args()
+if __name__ == "shark.parser" or __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="SHARK runner.")
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cpu",
+        help="Device on which shark_runner runs. options are cpu, cuda, and vulkan",
+    )
+    parser.add_argument(
+        "--repro_dir",
+        help="Directory to which module files will be saved for reproduction or debugging.",
+        type=dir_path,
+        default="./shark_tmp",
+    )
+    parser.add_argument(
+        "--enable_tf32",
+        type=bool,
+        default=False,
+        help="Enables TF32 precision calculations on supported GPUs.",
+    )
+    parser.add_argument(
+        "--model_config_path",
+        help="Directory to where the tuned model config file is located.",
+        default=None,
+    )
+    parser.add_argument(
+        "--num_warmup_iterations",
+        type=int,
+        default=5,
+        help="Run the model for the specified number of warmup iterations.",
+    )
+    parser.add_argument(
+        "--num_iterations",
+        type=int,
+        default=100,
+        help="Run the model for the specified number of iterations.",
+    )
+    parser.add_argument(
+        "--onnx_bench",
+        default=False,
+        action="store_true",
+        help="When enabled, pytest bench results will include ONNX benchmark results.",
+    )
+    parser.add_argument(
+        "--shark_prefix",
+        default="latest",
+        help="gs://shark_tank/<this_flag>/model_directories",
+    )
+    parser.add_argument(
+        "--update_tank",
+        default=False,
+        action="store_true",
+        help="When enabled, SHARK downloader will update local shark_tank if local hash is different from latest upstream hash.",
+    )
+    parser.add_argument(
+        "--local_tank_cache",
+        default="",
+        help="Specify where to save downloaded shark_tank artifacts. If this is not set, the default is ~/.local/shark_tank/.",
+    )
+    parser.add_argument(
+        "--dispatch_benchmarks",
+        default=None,
+        help='dispatches to return benchamrk data on.  use "All" for all, and None for none.',
+    )
+    parser.add_argument(
+        "--dispatch_benchmarks_dir",
+        default="temp_dispatch_benchmarks",
+        help='directory where you want to store dispatch data generated with "--dispatch_benchmarks"',
+    )
+    parser.add_argument(
+        "--enable_conv_transform",
+        default=False,
+        action="store",
+        help="Enables the --iree-flow-enable-conv-nchw-to-nhwc-transform flag.",
+    )
+    shark_args, unknown = parser.parse_known_args()
