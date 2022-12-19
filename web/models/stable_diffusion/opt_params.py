07bd30cf35d22242c8c867a8b809ff6a32a38ae1
Gaurav Shukla <gaurav@nod-labs.com>
2022-12-19 16:41:47 +0530

    [SD][web] Fix performance issues on shark scheduler
    Signed-Off-by: Gaurav Shukla <gaurav@nod-labs.com>

--- web/models/stable_diffusion/opt_params.py
+++ web/models/stable_diffusion/opt_params.py
@@ -7,10 +7,22 @@ from models.stable_diffusion.model_wrappers import (
 )
 from models.stable_diffusion.stable_args import args
 from models.stable_diffusion.utils import get_shark_model
+from shark.iree_utils.vulkan_utils import get_vulkan_triple_flag
 
 BATCH_SIZE = len(args.prompts)
 if BATCH_SIZE != 1:
     sys.exit("Only batch size 1 is supported.")
+
+# use tuned models only in the case of rdna3 cards.
+args.use_tuned = False
+if not args.iree_vulkan_target_triple:
+    vulkan_triple_flags = get_vulkan_triple_flag()
+    if vulkan_triple_flags and "rdna3" in vulkan_triple_flags:
+        args.use_tuned = True
+elif "rdna3" in args.iree_vulkan_target_triple:
+    args.use_tuned = True
+if args.use_tuned:
+    print("Using tuned models for rdna3 card")
 
 
 def get_unet():
