80943808605386f4e3c9449a76eb9c7779283275
Prashant Kumar <prashant@nod-labs.com>
2022-12-19 09:45:58 +0000

    Update VAE model and wrapper.

--- shark/examples/shark_inference/stable_diffusion/opt_params.py
+++ shark/examples/shark_inference/stable_diffusion/opt_params.py
@@ -1,7 +1,6 @@
 import sys
 from model_wrappers import (
     get_vae_mlir,
-    get_vae_encode_mlir,
     get_unet_mlir,
     get_clip_mlir,
 )
@@ -102,11 +101,11 @@ def get_vae():
             return get_shark_model(bucket, model_name, iree_flags)
         else:
             bucket = "gs://shark_tank/stable_diffusion"
-            model_name = "vae_8dec_fp16"
+            model_name = "vae_19dec_fp16"
             if args.version == "v2.1base":
-                model_name = "vae2base_8dec_fp16"
+                model_name = "vae2base_19dec_fp16"
             if args.version == "v2.1":
-                model_name = "vae2_14dec_fp16"
+                model_name = "vae2_19dec_fp16"
             iree_flags += [
                 "--iree-flow-enable-padding-linalg-ops",
                 "--iree-flow-linalg-ops-padding-size=32",
@@ -119,39 +118,6 @@ def get_vae():
     if args.precision == "fp32":
         bucket = "gs://shark_tank/stable_diffusion"
         model_name = "vae_1dec_fp32"
-        iree_flags += [
-            "--iree-flow-enable-conv-nchw-to-nhwc-transform",
-            "--iree-flow-enable-padding-linalg-ops",
-            "--iree-flow-linalg-ops-padding-size=16",
-        ]
-        if args.import_mlir:
-            return get_vae_mlir(model_name, iree_flags)
-        return get_shark_model(bucket, model_name, iree_flags)
-
-
-def get_vae_encode():
-    iree_flags = []
-    if len(args.iree_vulkan_target_triple) > 0:
-        iree_flags.append(
-            f"-iree-vulkan-target-triple={args.iree_vulkan_target_triple}"
-        )
-    if args.precision in ["fp16", "int8"]:
-        bucket = "gs://shark_tank/stable_diffusion"
-        model_name = "vae_encode_1dec_fp16"
-        if args.version == "v2":
-            model_name = "vae2_encode_29nov_fp16"
-        iree_flags += [
-            "--iree-flow-enable-conv-nchw-to-nhwc-transform",
-            "--iree-flow-enable-padding-linalg-ops",
-            "--iree-flow-linalg-ops-padding-size=32",
-        ]
-        if args.import_mlir:
-            return get_vae_encode_mlir(model_name, iree_flags)
-        return get_shark_model(bucket, model_name, iree_flags)
-
-    if args.precision == "fp32":
-        bucket = "gs://shark_tank/stable_diffusion"
-        model_name = "vae_encode_1dec_fp32"
         iree_flags += [
             "--iree-flow-enable-conv-nchw-to-nhwc-transform",
             "--iree-flow-enable-padding-linalg-ops",
