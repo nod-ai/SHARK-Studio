6694c0f16abfb0370cf2ea2e505408a83485de3f
xzuyn <draox@hotmail.com>
2023-01-16 22:10:37 -0500

    GUI make jpeg optimized and progressive

--- web/models/stable_diffusion/main.py
+++ web/models/stable_diffusion/main.py
@@ -84,7 +84,13 @@ def save_output_img(output_img):
     )
     if args.output_img_format == "jpg":
         out_img_path = Path(generated_imgs_path, f"{out_img_name}.jpg")
-        output_img.save(out_img_path, quality=95, subsampling=0)
+        output_img.save(
+            out_img_path,
+            quality=95,
+            subsampling=0,
+            optimize=True,
+            progressive=True,
+        )
     else:
         out_img_path = Path(generated_imgs_path, f"{out_img_name}.png")
         output_img.save(out_img_path, "PNG")
