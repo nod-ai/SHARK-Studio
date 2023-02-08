3159a6f3e12f49cc416f1a6731433c9da8120d97
PhaneeshB <b.phaneesh@gmail.com>
2023-02-08 13:08:01 +0530

    add support for img1img

--- apps/stable_diffusion/src/pipelines/__init__.py
+++ apps/stable_diffusion/src/pipelines/__init__.py
@@ -4,3 +4,6 @@ from apps.stable_diffusion.src.pipelines.pipeline_shark_stable_diffusion_txt2img
 from apps.stable_diffusion.src.pipelines.pipeline_shark_stable_diffusion_inpaint import (
     InpaintPipeline,
 )
+from apps.stable_diffusion.src.pipelines.pipeline_shark_stable_diffusion_img2img import (
+    Image2ImagePipeline,
+)
