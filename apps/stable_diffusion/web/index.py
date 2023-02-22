bc4459b6f48d711c8d20647e36fe1dce960997df
Gaurav Shukla <gaurav@nod-labs.com>
2023-02-23 00:31:18 +0530

    [SD] Add inpainting web UI (#1069)
    Signed-off-by: Gaurav Shukla <gaurav@nod-labs.com>

--- apps/stable_diffusion/web/index.py
+++ apps/stable_diffusion/web/index.py
@@ -27,11 +27,11 @@ def resource_path(relative_path):
 
 dark_theme = resource_path("ui/css/sd_dark_theme.css")
 
-from apps.stable_diffusion.web.ui import txt2img_web, img2img_web
+from apps.stable_diffusion.web.ui import txt2img_web, img2img_web, inpaint_web
 
 sd_web = gr.TabbedInterface(
-    [txt2img_web, img2img_web],
-    ["Text-to-Image", "Image-to-Image"],
+    [txt2img_web, img2img_web, inpaint_web],
+    ["Text-to-Image", "Image-to-Image", "Inpainting"],
     css=dark_theme,
 )
 
