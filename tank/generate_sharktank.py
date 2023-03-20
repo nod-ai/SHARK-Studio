d105246b9c6207a06f2158f9bdc66af8679d9032
mariecwhite <mariewhite@google.com>
2023-03-20 16:14:53 -0700

    Fix t5 models

--- tank/generate_sharktank.py
+++ tank/generate_sharktank.py
@@ -150,7 +150,7 @@ def save_tf_model(tf_model_list, local_tank_cache):
             input = None
             print(f"Generating artifacts for model {tf_model_name}")
             if model_type == "hf":
-                model, input, _ = get_masked_lm_model(tf_model_name)
+                model, input, _ = get_causal_lm_model(tf_model_name)
             elif model_type == "img":
                 model, input, _ = get_causal_image_model(tf_model_name)
             elif model_type == "keras":
@@ -159,8 +159,6 @@ def save_tf_model(tf_model_list, local_tank_cache):
                 model, input, _ = get_TFhf_model(tf_model_name)
             elif model_type == "tfhf_seq2seq":
                 model, input, _ = get_tfhf_seq2seq_model(tf_model_name)
-            elif model_type == "hf_causallm":
-                model, input, _ = get_causal_lm_model(tf_model_name)
 
             tf_model_name = tf_model_name.replace("/", "_")
             tf_model_dir = os.path.join(
