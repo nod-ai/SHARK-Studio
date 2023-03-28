5bb7846227a393536681dac37eaa184db4cdc204
Daniel Garvey <34486624+dan-garvey@users.noreply.github.com>
2023-03-28 11:15:21 -0500

    single entry point exe for all cli apps (#1158)
    usage:
    add --app="img2img" (or "inpaint" "outpaint" "txt2img")

--- apps/stable_diffusion/scripts/outpaint.py
+++ apps/stable_diffusion/scripts/outpaint.py
@@ -200,7 +200,7 @@ def outpaint_inf(
     return generated_imgs, text_output
 
 
-if __name__ == "__main__":
+def main():
     if args.clear_all:
         clear_all()
 
@@ -299,3 +299,7 @@ if __name__ == "__main__":
         }
         save_output_img(generated_imgs[0], seed, extra_info)
         print(text_output)
+
+
+if __name__ == "__main__":
+    main()
