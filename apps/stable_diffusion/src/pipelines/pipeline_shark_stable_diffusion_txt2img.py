d2a09945848727ba0cd420133d11c778aabd2ae4
dan <danimal197@gmail.com>
2023-04-03 21:30:35 -0500

    remove 1/1 tqdm progress bar

--- apps/stable_diffusion/src/pipelines/pipeline_shark_stable_diffusion_txt2img.py
+++ apps/stable_diffusion/src/pipelines/pipeline_shark_stable_diffusion_txt2img.py
@@ -1,5 +1,4 @@
 import torch
-from tqdm.auto import tqdm
 import numpy as np
 from random import randint
 from transformers import CLIPTokenizer
@@ -128,7 +127,7 @@ class Text2ImagePipeline(StableDiffusionPipeline):
 
         # Img latents -> PIL images
         all_imgs = []
-        for i in tqdm(range(0, latents.shape[0], batch_size)):
+        for i in range(0, latents.shape[0], batch_size):
             imgs = self.decode_latents(
                 latents=latents[i : i + batch_size],
                 use_base_vae=use_base_vae,
