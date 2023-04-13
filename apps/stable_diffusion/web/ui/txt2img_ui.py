1b3f468c04091d638ede339c24bd8e58f1a08bee
m68k-fr <43201437+m68k-fr@users.noreply.github.com>
2023-04-14 01:40:42 +0200

    [Web] Style Fixes for Gradio V3.25.0 (#1300)

--- apps/stable_diffusion/web/ui/txt2img_ui.py
+++ apps/stable_diffusion/web/ui/txt2img_ui.py
@@ -394,7 +394,7 @@ with gr.Blocks(title="Text-to-Image") as txt2img_web:
                         label="Generated images",
                         show_label=False,
                         elem_id="gallery",
-                    ).style(grid=[2])
+                    ).style(columns=[2], object_fit="contain")
                     std_output = gr.Textbox(
                         value="Nothing to show.",
                         lines=1,
--- apps/stable_diffusion/web/ui/upscaler_ui.py
+++ apps/stable_diffusion/web/ui/upscaler_ui.py
@@ -204,7 +204,7 @@ with gr.Blocks(title="Upscaler") as upscaler_web:
                         label="Generated images",
                         show_label=False,
                         elem_id="gallery",
-                    ).style(grid=[2])
+                    ).style(columns=[2], object_fit="contain")
                     std_output = gr.Textbox(
                         value="Nothing to show.",
                         lines=1,
