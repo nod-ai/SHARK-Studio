0a9e6da266aeeeda2121138cb794e22a949fef83
Ean Garvey <87458719+monorimet@users.noreply.github.com>
2023-04-18 23:07:29 -0500

    Update populate_sharktank_ci.sh

--- build_tools/populate_sharktank_ci.sh
+++ build_tools/populate_sharktank_ci.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-IMPORTER=1 BENCHMARK=1 ./setup_venv.sh
-source $GITHUB_WORKSPACE/shark.venv/bin/activate
+source $GITHUB_WORKSPACE/iree.venv/bin/activate
 python tank/generate_sharktank.py
