2dbb2a17dbac70ca3e436d914bd871fbbe9bd651
Ean Garvey <garveyej@gmail.com>
2023-04-21 12:27:23 -0500

    Fix inference object imports for SD apps.

--- apps/stable_diffusion/scripts/__init__.py
+++ apps/stable_diffusion/scripts/__init__.py
@@ -1,4 +1,1 @@
-from apps.stable_diffusion.scripts.inpaint import inpaint_inf
-from apps.stable_diffusion.scripts.outpaint import outpaint_inf
-from apps.stable_diffusion.scripts.upscaler import upscaler_inf
 from apps.stable_diffusion.scripts.train_lora_word import lora_train
