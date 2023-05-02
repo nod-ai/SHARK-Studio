20114deea0650eff2874de3a19f8f92cf25bf01d
Boian Petkantchin <boian@nod-labs.com>
2023-05-02 15:21:56 -0700

    In MiniLM JAX example verify MLIR result against JAX

--- shark/examples/shark_inference/minilm_jax.py
+++ shark/examples/shark_inference/minilm_jax.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, FlaxAutoModel
 import torch
 import jax
-from typing import Union, Dict, List
+from typing import Union, Dict, List, Any
 import numpy as np
 from shark.shark_inference import SharkInference
 import io
@@ -36,18 +36,38 @@ def get_sample_input():
     )
 
 
-def export_to_mlir(sample_input: NumpyTree):
-    model = FlaxAutoModel.from_pretrained("microsoft/MiniLM-L12-H384-uncased")
-    model_mlir = jax.jit(model).lower(**sample_input).compiler_ir()
-    return str(model_mlir).encode()
+def get_jax_model():
+    return FlaxAutoModel.from_pretrained("microsoft/MiniLM-L12-H384-uncased")
+
+
+def export_jax_to_mlir(jax_model: Any, sample_input: NumpyTree):
+    model_mlir = jax.jit(jax_model).lower(**sample_input).compiler_ir()
+    byte_stream = io.BytesIO()
+    model_mlir.operation.write_bytecode(file=byte_stream)
+    return byte_stream.getvalue()
+
+
+def assert_array_list_allclose(x, y, *args, **kwargs):
+    assert len(x) == len(y)
+    for a, b in zip(x, y):
+        np.testing.assert_allclose(
+            np.asarray(a), np.asarray(b), *args, **kwargs
+        )
 
 
 sample_input = get_sample_input()
-mlir = export_to_mlir(sample_input)
+jax_model = get_jax_model()
+mlir = export_jax_to_mlir(jax_model, sample_input)
 
 # Compile and load module.
 shark_inference = SharkInference(mlir_module=mlir, mlir_dialect="mhlo")
 shark_inference.compile()
 
 # Run main function.
-print(shark_inference("main", jax.tree_util.tree_flatten(sample_input)[0]))
+result = shark_inference("main", jax.tree_util.tree_flatten(sample_input)[0])
+
+# Run JAX model.
+reference_result = jax.tree_util.tree_flatten(jax_model(**sample_input))[0]
+
+# Verify result.
+assert_array_list_allclose(result, reference_result, atol=1e-5)
