05dcb8d31af36f3a4c7e63ef95746461f665e2ce
Ean Garvey <ean@nod-labs.com>
2023-06-05 21:49:43 +0000

    Add OPT1.3b test script.

--- tank/examples/opt/opt_causallm_torch_test.py
+++ tank/examples/opt/opt_causallm_torch_test.py
@@ -1,8 +1,11 @@
 import unittest
 
+import os
 import pytest
 import torch_mlir
-from shark_hf_opt import OPTModel
+import torch
+import numpy as np
+from shark_hf_opt import OPTForCausalLM
 from shark.iree_utils._common import check_device_drivers, device_driver_info
 from shark.shark_inference import SharkInference
 from tank.model_utils import compare_tensors
@@ -23,7 +26,9 @@ class OPTModuleTester:
         tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
         # config = OPTConfig()
         # opt_model = OPTModel(config)
-        opt_model = OPTModel.from_pretrained(model_name)
+        opt_model = OPTForCausalLM.from_pretrained(
+            model_name, return_dict=False
+        )
         opt_model.eval()
 
         inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
@@ -31,19 +36,30 @@ class OPTModuleTester:
             inputs.data["input_ids"],
             inputs.data["attention_mask"],
         )
+        np.save("opt_inputs.npy", input_ids.detach())
+        mlir_path = "./OPT1-3b_causallm_torch.mlir"
+        if os.path.isfile(mlir_path):
+            with open(mlir_path, "r") as f:
+                model_mlir = f.read()
+            print(f"Loaded .mlir from {mlir_path}")
+        else:
+            module = torch_mlir.compile(
+                opt_model,
+                input_ids,
+                output_type=torch_mlir.OutputType.LINALG_ON_TENSORS,
+                use_tracing=True,
+            )
 
-        module = torch_mlir.compile(
-            opt_model,
-            (input_ids, attention_mask),
-            output_type=torch_mlir.OutputType.LINALG_ON_TENSORS,
-            use_tracing=True,
-        )
+            model_mlir = module.operation.get_asm(
+                large_elements_limit=None, enable_debug_info=True
+            )
 
-        model_mlir = module.operation.get_asm(
-            large_elements_limit=None, enable_debug_info=True
-        )
+            with open(mlir_path, "w") as f:
+                f.write(model_mlir)
+            print(f"Saved mlir at {mlir_path}")
+
         func_name = "forward"
-        act_out = opt_model(input_ids, attention_mask).detach()
+        act_out = opt_model(input_ids, return_dict=False)
 
         # mlir_importer = SharkImporter(
         #    model,
@@ -61,8 +77,20 @@ class OPTModuleTester:
             is_benchmark=self.benchmark,
         )
         shark_module.compile()
-        results = shark_module("forward", (input_ids, attention_mask))
-        assert compare_tensors(act_out, results)
+        results = shark_module("forward", (input_ids,))
+        print(
+            "SHARK logits have shape: ",
+            str(results[0].shape) + " : " + str(results[0]),
+        )
+        print(
+            "PyTorch logits have shape: "
+            + str(act_out[0].shape)
+            + " : "
+            + str(act_out[0])
+        )
+        # exp_out = tokenizer.decode(act_out[0][0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+        # shark_out = tokenizer.decode(results[0][0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+        assert compare_tensors(act_out[0].detach(), results[0])
 
         if self.benchmark:
             shark_module.shark_runner.benchmark_all_csv(
