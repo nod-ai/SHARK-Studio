34400255713568d10befc9e077a8beb955ca3125
AyaanShah2204 <ayaanshah2204@gmail.com>
2023-06-20 09:58:45 -0700

    fixed linter errors

--- rest_api_tests/api_test.py
+++ rest_api_tests/api_test.py
@@ -3,8 +3,8 @@ from PIL import Image
 import base64
 from io import BytesIO
 
-def upscaler_test():
 
+def upscaler_test():
     # Define values here
     prompt = ""
     negative_prompt = ""
@@ -14,11 +14,13 @@ def upscaler_test():
     steps = 50
     noise_level = 10
     cfg_scale = 7
-    image_path = r"../dog.png"
+    image_path = r"./dog.png"
 
     # Converting Image to base64
-    img_file = open(image_path, 'rb')
-    init_images = ["data:image/png;base64," + base64.b64encode(img_file.read()).decode()]
+    img_file = open(image_path, "rb")
+    init_images = [
+        "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+    ]
 
     url = "http://127.0.0.1:8080/sdapi/v1/upscaler"
 
@@ -29,24 +31,23 @@ def upscaler_test():
     }
 
     data = {
-        "prompt" : prompt,
-        "negative_prompt" : negative_prompt,
-        "seed" : seed,
-        "height" : height,
-        "width" : width,
-        "steps" : steps,
-        "noise_level" : noise_level,
-        "cfg_scale" : cfg_scale,
-        "init_images" : init_images
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "seed": seed,
+        "height": height,
+        "width": width,
+        "steps": steps,
+        "noise_level": noise_level,
+        "cfg_scale": cfg_scale,
+        "init_images": init_images,
     }
 
-    res = requests.post(url = url, json= data, headers= headers, timeout=1000)
+    res = requests.post(url=url, json=data, headers=headers, timeout=1000)
 
     print(f"response from server was : {res.status_code}")
 
 
 def img2img_test():
-
     # Define values here
     prompt = "Paint a rabbit riding on the dog"
     negative_prompt = "ugly, bad art, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, tiling, signature, cut off, draft"
@@ -56,13 +57,13 @@ def img2img_test():
     steps = 50
     denoising_strength = 0.75
     cfg_scale = 7
-    image_path = r"../dog.png"
-
+    image_path = r"./dog.png"
 
     # Converting Image to Base64
-    img_file = open(image_path, 'rb')
-    init_images = ["data:image/png;base64," + base64.b64encode(img_file.read()).decode()]
-
+    img_file = open(image_path, "rb")
+    init_images = [
+        "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+    ]
 
     url = "http://127.0.0.1:8080/sdapi/v1/img2img"
 
@@ -81,10 +82,10 @@ def img2img_test():
         "steps": steps,
         "denoising_strength": denoising_strength,
         "cfg_scale": cfg_scale,
-        "seed": seed
+        "seed": seed,
     }
 
-    res = requests.post(url = url, json= data, headers= headers, timeout=1000)
+    res = requests.post(url=url, json=data, headers=headers, timeout=1000)
 
     print(f"response from server was : {res.status_code}")
 
@@ -99,6 +100,7 @@ def img2img_test():
     # response_img = Image.open(im_file)
     # print("Saving Response Image to: response_img")
     # response_img.save('bla.png')
+
 
 img2img_test()
 upscaler_test()
