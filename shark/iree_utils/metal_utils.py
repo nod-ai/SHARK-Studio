948cef51c70e5d1c1e3a6b4abc736d9b05e1689c
ranivrsv <ranvirsv@gmail.com>
2023-06-20 11:51:36 -0700

    Fixing iree-metal-target-platform

--- shark/iree_utils/metal_utils.py
+++ shark/iree_utils/metal_utils.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The Nod Team. All rights reserved.
+# Copyright 2023 The Nod Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -56,68 +56,11 @@ def get_metal_target_triple(device_name):
     Returns:
         str or None: target triple or None if no match found for given name
     """
-    system_os = get_os_name()
     # Apple Targets
     if all(x in device_name for x in ("Apple", "M1")):
         triple = "m1-moltenvk-macos"
     elif all(x in device_name for x in ("Apple", "M2")):
         triple = "m1-moltenvk-macos"
-
-    # Nvidia Targets
-    elif all(x in device_name for x in ("RTX", "2080")):
-        triple = f"turing-rtx2080-{system_os}"
-    elif all(x in device_name for x in ("A100", "SXM4")):
-        triple = f"ampere-a100-{system_os}"
-    elif all(x in device_name for x in ("RTX", "3090")):
-        triple = f"ampere-rtx3090-{system_os}"
-    elif all(x in device_name for x in ("RTX", "3080")):
-        triple = f"ampere-rtx3080-{system_os}"
-    elif all(x in device_name for x in ("RTX", "3070")):
-        triple = f"ampere-rtx3070-{system_os}"
-    elif all(x in device_name for x in ("RTX", "3060")):
-        triple = f"ampere-rtx3060-{system_os}"
-    elif all(x in device_name for x in ("RTX", "3050")):
-        triple = f"ampere-rtx3050-{system_os}"
-    # We use ampere until lovelace target triples are plumbed in.
-    elif all(x in device_name for x in ("RTX", "4090")):
-        triple = f"ampere-rtx4090-{system_os}"
-    elif all(x in device_name for x in ("RTX", "4080")):
-        triple = f"ampere-rtx4080-{system_os}"
-    elif all(x in device_name for x in ("RTX", "4070")):
-        triple = f"ampere-rtx4070-{system_os}"
-    elif all(x in device_name for x in ("RTX", "4000")):
-        triple = f"turing-rtx4000-{system_os}"
-    elif all(x in device_name for x in ("RTX", "5000")):
-        triple = f"turing-rtx5000-{system_os}"
-    elif all(x in device_name for x in ("RTX", "6000")):
-        triple = f"turing-rtx6000-{system_os}"
-    elif all(x in device_name for x in ("RTX", "8000")):
-        triple = f"turing-rtx8000-{system_os}"
-    elif all(x in device_name for x in ("TITAN", "RTX")):
-        triple = f"turing-titanrtx-{system_os}"
-    elif all(x in device_name for x in ("GTX", "1060")):
-        triple = f"pascal-gtx1060-{system_os}"
-    elif all(x in device_name for x in ("GTX", "1070")):
-        triple = f"pascal-gtx1070-{system_os}"
-    elif all(x in device_name for x in ("GTX", "1080")):
-        triple = f"pascal-gtx1080-{system_os}"
-
-    # Amd Targets
-    # Linux: Radeon RX 7900 XTX
-    # Windows: AMD Radeon RX 7900 XTX
-    elif all(x in device_name for x in ("RX", "7900")):
-        triple = f"rdna3-7900-{system_os}"
-    elif all(x in device_name for x in ("AMD", "PRO", "W7900")):
-        triple = f"rdna3-w7900-{system_os}"
-    elif any(x in device_name for x in ("AMD", "Radeon")):
-        triple = f"rdna2-unknown-{system_os}"
-    # Intel Targets
-    elif any(x in device_name for x in ("A770", "A750")):
-        triple = f"arc-770-{system_os}"
-
-    # Adreno Targets
-    elif all(x in device_name for x in ("Adreno", "740")):
-        triple = f"adreno-a740-{system_os}"
 
     else:
         triple = None
@@ -126,7 +69,7 @@ def get_metal_target_triple(device_name):
 
 def get_metal_triple_flag(device_name="", device_num=0, extra_args=[]):
     for flag in extra_args:
-        if "-iree-metal-target-triple=" in flag:
+        if "-iree-metal-target-platform=" in flag:
             print(f"Using target triple {flag.split('=')[1]}")
             return None
 
@@ -154,7 +97,7 @@ def get_iree_metal_args(device_num=0, extra_args=[]):
     res_metal_flag = []
     metal_triple_flag = None
     for arg in extra_args:
-        if "-iree-metal-target-triple=" in arg:
+        if "-iree-metal-target-platform=" in arg:
             print(f"Using target triple {arg} from command line args")
             meatal_triple_flag = arg
             break
