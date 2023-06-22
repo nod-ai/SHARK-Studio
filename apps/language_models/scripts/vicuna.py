8822b9acd7e01f212f37f40dcc7e7ba8e9fafc36
Eliasj42 <46754803+Eliasj42@users.noreply.github.com>
2023-06-22 15:40:35 -0700

    added ability to use config file to shard vicuna (#1565)
    Co-authored-by: Elias Joseph <elias@nod-labs.com>

--- apps/language_models/scripts/vicuna.py
+++ apps/language_models/scripts/vicuna.py
@@ -3,6 +3,10 @@ from pathlib import Path
 from apps.language_models.src.pipelines import vicuna_pipeline as vp
 from apps.language_models.src.pipelines import vicuna_sharded_pipeline as vsp
 import torch
+import json
+
+if __name__ == "__main__":
+    import gc
 
 
 parser = argparse.ArgumentParser(
@@ -55,6 +59,12 @@ parser.add_argument(
     help="Run model in cli mode",
 )
 
+parser.add_argument(
+    "--config",
+    default=None,
+    help="configuration file",
+)
+
 if __name__ == "__main__":
     args, unknown = parser.parse_known_args()
 
@@ -80,6 +90,7 @@ if __name__ == "__main__":
             if args.second_vicuna_vmfb_path is None
             else Path(args.second_vicuna_vmfb_path)
         )
+
         vic = vp.Vicuna(
             "vicuna",
             device=args.device,
@@ -91,15 +102,20 @@ if __name__ == "__main__":
             load_mlir_from_shark_tank=args.load_mlir_from_shark_tank,
         )
     else:
+        if args.config is not None:
+            config_file = open(args.config)
+            config_json = json.load(config_file)
+            config_file.close()
+        else:
+            config_json = None
         vic = vsp.Vicuna(
             "vicuna",
             device=args.device,
             precision=args.precision,
+            config_json=config_json,
         )
     prompt_history = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n"
     prologue_prompt = "ASSISTANT:\n"
-
-    import gc
 
     while True:
         # TODO: Add break condition from user input
