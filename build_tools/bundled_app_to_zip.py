f8d35d9e762941607dbc94f5ff0fe730baa0adda
AyaanShah2204 <ayaanshah2204@gmail.com>
2023-07-08 23:51:17 -0700

    Removed zip script

--- build_tools/bundled_app_to_zip.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-output_filename = "./studio_bundle_zipped"
-directory_path = "./dist/studio_bundle"
-
-try:
-    shutil.make_archive(output_filename, "zip", directory_path)
-except:
-    print("An exception occured creating the ZIP file")
