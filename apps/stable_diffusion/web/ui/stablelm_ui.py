38e5b62d80cd457a778bce7f70bd027d415581dc
PhaneeshB <b.phaneesh@gmail.com>
2023-07-11 18:24:55 +0530

    adapt UI to send model details to pipeline

--- apps/stable_diffusion/web/ui/stablelm_ui.py
+++ apps/stable_diffusion/web/ui/stablelm_ui.py
@@ -7,18 +7,6 @@ from transformers import (
 )
 from apps.stable_diffusion.web.ui.utils import available_devices
 
-start_message = (
-    "<|SYSTEM|># StableLM Tuned (Alpha version)"
-    "\n- StableLM is a helpful and harmless open-source AI language model "
-    "developed by StabilityAI."
-    "\n- StableLM is excited to be able to help the user, but will refuse "
-    "to do anything that could be considered harmful to the user."
-    "\n- StableLM is more than just an information source, StableLM is also "
-    "able to write poetry, short stories, and make jokes."
-    "\n- StableLM will refuse to participate in anything that "
-    "could harm a human."
-)
-
 
 def user(message, history):
     # Append the user's message to the conversation history
@@ -29,26 +17,66 @@ sharkModel = 0
 sharded_model = 0
 vicuna_model = 0
 
-
-start_message_vicuna = (
-    "A chat between a curious user and an artificial intelligence assistant. "
-    "The assistant gives helpful, detailed, and polite answers to the user's "
-    "questions.\n"
-)
 past_key_values = None
+
+model_map = {
+    "vicuna1p3": "lmsys/vicuna-7b-v1.3",
+    "vicuna": "TheBloke/vicuna-7B-1.1-HF",
+    "StableLM": "stabilityai/stablelm-tuned-alpha-3b",
+}
+
+# NOTE: Each `model_name` should have its own start message
+start_message = {
+    "StableLM": (
+        "<|SYSTEM|># StableLM Tuned (Alpha version)"
+        "\n- StableLM is a helpful and harmless open-source AI language model "
+        "developed by StabilityAI."
+        "\n- StableLM is excited to be able to help the user, but will refuse "
+        "to do anything that could be considered harmful to the user."
+        "\n- StableLM is more than just an information source, StableLM is also "
+        "able to write poetry, short stories, and make jokes."
+        "\n- StableLM will refuse to participate in anything that "
+        "could harm a human."
+    ),
+    "vicuna": (
+        "A chat between a curious user and an artificial intelligence assistant. "
+        "The assistant gives helpful, detailed, and polite answers to the user's "
+        "questions.\n"
+    ),
+    "vicuna1p3": (
+        "A chat between a curious user and an artificial intelligence assistant. "
+        "The assistant gives helpful, detailed, and polite answers to the user's "
+        "questions.\n"
+    ),
+}
+
+
+def create_prompt(model_name, history):
+    system_message = start_message[model_name]
+
+    if model_name in ["StableLM", "vicuna", "vicuna1p3"]:
+        conversation = "".join(["".join(["<|USER|>" + item[0], "<|ASSISTANT|>" + item[1]]) for item in history])
+    else:
+        conversation = "".join(["".join([item[0], item[1]]) for item in history])
+
+    msg = system_message + conversation
+    msg = msg.strip()
+    return msg
 
 
 def chat(curr_system_message, history, model, device, precision):
-    print(f"In chat for {model}")
     global sharded_model
     global past_key_values
     global vicuna_model
-    if "vicuna" in model:
+
+    model_name, model_path = list(map(str.strip, model.split("=>")))
+    print(f"In chat for {model_name}")
+
+    if model_name in ["vicuna", "vicuna1p3"]:
         from apps.language_models.scripts.vicuna import (
             UnshardedVicuna,
         )
 
-        curr_system_message = start_message_vicuna
         if vicuna_model == 0:
             if "cuda" in device:
                 device = "cuda"
@@ -61,18 +89,12 @@ def chat(curr_system_message, history, model, device, precision):
             else:
                 print("unrecognized device")
             vicuna_model = UnshardedVicuna(
-                "vicuna",
-                hf_model_path=model,
+                model_name,
+                hf_model_path=model_path,
                 device=device,
                 precision=precision,
             )
-        messages = curr_system_message + "".join(
-            [
-                "".join(["<|USER|>" + item[0], "<|ASSISTANT|>" + item[1]])
-                for item in history
-            ]
-        )
-        prompt = messages.strip()
+        prompt = create_prompt(model_name, history)
         print("prompt = ", prompt)
 
         for partial_text in vicuna_model.generate(prompt):
@@ -89,23 +111,14 @@ def chat(curr_system_message, history, model, device, precision):
 
     if sharkModel == 0:
         # max_new_tokens=512
-        shark_slm = SharkStableLM(
-            "StableLM"
-        )  # pass elements from UI as required
+        shark_slm = SharkStableLM(model_name)  # pass elements from UI as required
 
     # Construct the input message string for the model by concatenating the
     # current system message and conversation history
     if len(curr_system_message.split()) > 160:
         print("clearing context")
-        curr_system_message = start_message
-    messages = curr_system_message + "".join(
-        [
-            "".join(["<|USER|>" + item[0], "<|ASSISTANT|>" + item[1]])
-            for item in history
-        ]
-    )
-
-    generate_kwargs = dict(prompt=messages)
+    prompt = create_prompt(model_name, history)
+    generate_kwargs = dict(prompt=prompt)
 
     words_list = shark_slm.generate(**generate_kwargs)
 
@@ -122,13 +135,11 @@ def chat(curr_system_message, history, model, device, precision):
 
 with gr.Blocks(title="Chatbot") as stablelm_chat:
     with gr.Row():
+        model_choices = list(map(lambda x: f"{x[0]: <10} => {x[1]}", model_map))
         model = gr.Dropdown(
             label="Select Model",
-            value="TheBloke/vicuna-7B-1.1-HF",
-            choices=[
-                "stabilityai/stablelm-tuned-alpha-3b",
-                "TheBloke/vicuna-7B-1.1-HF",
-            ],
+            value=model_choices[0],
+            choices=model_choices,
         )
         supported_devices = available_devices
         enabled = len(supported_devices) > 0
@@ -138,9 +149,7 @@ with gr.Blocks(title="Chatbot") as stablelm_chat:
         print(supported_devices)
         device = gr.Dropdown(
             label="Device",
-            value=supported_devices[0]
-            if enabled
-            else "Only CUDA Supported for now",
+            value=supported_devices[0] if enabled else "Only CUDA Supported for now",
             choices=supported_devices,
             interactive=enabled,
         )
@@ -170,21 +179,15 @@ with gr.Blocks(title="Chatbot") as stablelm_chat:
                 submit = gr.Button("Submit", interactive=enabled)
                 stop = gr.Button("Stop", interactive=enabled)
                 clear = gr.Button("Clear", interactive=enabled)
-    system_msg = gr.Textbox(
-        start_message, label="System Message", interactive=False, visible=False
-    )
+    system_msg = gr.Textbox(start_message, label="System Message", interactive=False, visible=False)
 
-    submit_event = msg.submit(
-        fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False
-    ).then(
+    submit_event = msg.submit(fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False).then(
         fn=chat,
         inputs=[system_msg, chatbot, model, device, precision],
         outputs=[chatbot],
         queue=True,
     )
-    submit_click_event = submit.click(
-        fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False
-    ).then(
+    submit_click_event = submit.click(fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False).then(
         fn=chat,
         inputs=[system_msg, chatbot, model, device, precision],
         outputs=[chatbot],
