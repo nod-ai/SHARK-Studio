c61b6f8d6589b167a3743adb33981cca07ae13e6
Dom <97384583+tosemml@users.noreply.github.com>
2023-09-11 09:30:56 -0700

    Code refactoring  (#1817)
    * use join
    
    * fix bug
    
    * further code optimizations
    
    ---------
    
    Co-authored-by: Daniel Garvey <34486624+dan-garvey@users.noreply.github.com>

--- shark/iree_utils/vulkan_target_env_utils.py
+++ shark/iree_utils/vulkan_target_env_utils.py
@@ -57,11 +57,7 @@ def get_version(triple):
 @functools.cache
 def get_extensions(triple):
     def make_ext_list(ext_list):
-        res = ""
-        for e in ext_list:
-            res += e + ", "
-        res = f"[{res[:-2]}]"
-        return res
+        return ", ".join(ext_list)
 
     arch, product, os = triple
     if arch == "m1":
