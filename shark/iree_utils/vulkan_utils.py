a38cc9d2168606fdf3c6fc89aa2ab8f0fbfe950c
powderluv <powderluv@users.noreply.github.com>
2023-10-04 20:33:07 -0700

    Update vulkan_utils.py for Radeon 780m igpu (#1866)

--- shark/iree_utils/vulkan_utils.py
+++ shark/iree_utils/vulkan_utils.py
@@ -119,6 +119,8 @@ def get_vulkan_target_triple(device_name):
     # Windows: AMD Radeon RX 7900 XTX
     elif all(x in device_name for x in ("RX", "7900")):
         triple = f"rdna3-7900-{system_os}"
+    elif all(x in device_name for x in ("Radeon", "780M")):
+        triple = f"rdna3-780m-{system_os}"
     elif all(x in device_name for x in ("AMD", "PRO", "W7900")):
         triple = f"rdna3-w7900-{system_os}"
     elif any(x in device_name for x in ("AMD", "Radeon")):
