100d9d30ebd4388057946ccbfe103ce0685facbe
Sungsoon Cho <sungsoon.cho@gmail.com>
2023-10-24 17:31:15 -0700

    Add opt_causallm_samples.py.

---
+++ tank/examples/opt/opt_util.py
@@ -0,0 +1,12 @@
+PROMPTS = [
+    "What is the meaning of life?",
+    "Tell me something you don't know.",
+    "What does Xilinx do?",
+    "What is the mass of earth?",
+    "What is a poem?",
+    "What is recursion?",
+    "Tell me a one line joke.",
+    "Who is Gilgamesh?",
+    "Tell me something about cryptocurrency.",
+    "How did it all begin?",
+]
