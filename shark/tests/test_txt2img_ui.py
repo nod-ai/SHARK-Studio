35068628fc42c4d544c45942789223c6e92ad3ca
Richard Pastirčák <richard.pastircak@student.tuke.sk>
2023-12-06 00:52:00 +0100

    #1843 reformating units test

--- shark/tests/test_txt2img_ui.py
+++ shark/tests/test_txt2img_ui.py
@@ -1,25 +1,37 @@
 import unittest
 from unittest.mock import mock_open, patch
 
-from apps.stable_diffusion.web.ui.txt2img_ui import export_settings, load_settings, all_gradio_labels
+from apps.stable_diffusion.web.ui.txt2img_ui import (
+    export_settings,
+    load_settings,
+    all_gradio_labels,
+)
 
 
 class TestExportSettings(unittest.TestCase):
-
-    @patch('builtins.open', new_callable=mock_open)
-    @patch('json.dump')
+    @patch("builtins.open", new_callable=mock_open)
+    @patch("json.dump")
     def test_export_settings(self, mock_json_dump, mock_file):
-        test_values = ['value1', 'value2', 'value3']
+        test_values = ["value1", "value2", "value3"]
         expected_output = {
-            'txt2img': {label: value for label, value in zip(all_gradio_labels, test_values)}
+            "txt2img": {
+                label: value
+                for label, value in zip(all_gradio_labels, test_values)
+            }
         }
 
         export_settings(*test_values)
-        mock_file.assert_called_once_with('./ui/settings.json', 'w')
-        mock_json_dump.assert_called_once_with(expected_output, mock_file(), indent=4)
+        mock_file.assert_called_once_with("./ui/settings.json", "w")
+        mock_json_dump.assert_called_once_with(
+            expected_output, mock_file(), indent=4
+        )
 
-    @patch('apps.stable_diffusion.web.ui.txt2img_ui.json.load')
-    @patch('builtins.open', new_callable=mock_open, read_data='{"txt2img": {"some_setting": "some_value"}}')
+    @patch("apps.stable_diffusion.web.ui.txt2img_ui.json.load")
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data='{"txt2img": {"some_setting": "some_value"}}',
+    )
     def test_load_settings_file_exists(self, mock_file, mock_json_load):
         mock_json_load.return_value = {
             "txt2img": {
@@ -32,16 +44,16 @@ class TestExportSettings(unittest.TestCase):
         self.assertEqual(settings[0], "custom_model_value")
         self.assertEqual(settings[1], "custom_vae_value")
 
-    @patch('apps.stable_diffusion.web.ui.txt2img_ui.json.load')
-    @patch('builtins.open', side_effect=FileNotFoundError)
+    @patch("apps.stable_diffusion.web.ui.txt2img_ui.json.load")
+    @patch("builtins.open", side_effect=FileNotFoundError)
     def test_load_settings_file_not_found(self, mock_file, mock_json_load):
         settings = load_settings()
 
         default_lora_weights = "None"
         self.assertEqual(settings[4], default_lora_weights)
 
-    @patch('apps.stable_diffusion.web.ui.txt2img_ui.json.load')
-    @patch('builtins.open', new_callable=mock_open, read_data='{}')
+    @patch("apps.stable_diffusion.web.ui.txt2img_ui.json.load")
+    @patch("builtins.open", new_callable=mock_open, read_data="{}")
     def test_load_settings_key_error(self, mock_file, mock_json_load):
         mock_json_load.return_value = {}
 
