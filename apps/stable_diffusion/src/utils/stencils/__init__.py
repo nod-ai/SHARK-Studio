75ef5cb1e0d1f07efdbfb79d439c0097940cfd76
dan <danimal197@gmail.com>
2023-12-14 21:46:06 -0600

    the purge

--- apps/stable_diffusion/src/utils/stencils/__init__.py
+++
@@ -1,3 +0,0 @@
-from apps.stable_diffusion.src.utils.stencils.canny import CannyDetector
-from apps.stable_diffusion.src.utils.stencils.openpose import OpenposeDetector
-from apps.stable_diffusion.src.utils.stencils.zoe import ZoeDetector
--- apps/stable_diffusion/web/utils/metadata/__init__.py
+++
@@ -1,6 +0,0 @@
-from .png_metadata import (
-    import_png_metadata,
-)
-from .display import (
-    displayable_metadata,
-)
