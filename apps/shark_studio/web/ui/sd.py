ac48b843e73601f2ce4490445440b34355171392
Ean Garvey <garveyej@gmail.com>
2024-06-02 14:00:45 -0500

    actually reduce steps

--- apps/shark_studio/web/ui/sd.py
+++ apps/shark_studio/web/ui/sd.py
@@ -456,7 +456,7 @@ with gr.Blocks(title="Stable Diffusion") as sd_element:
                 with gr.Row():
                     steps = gr.Dropdown(
                         value=cmd_opts.steps,
-                        choices=[1, 2, 3, 4],
+                        choices=[1, 2, 3],
                         label="\U0001F3C3\U0000FE0F Steps",
                         allow_custom_value=True,
                     )
